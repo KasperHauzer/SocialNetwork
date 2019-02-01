8cecd56d11e0f3ac4d5d0ced34c229e2b0466e51
Adam Arsanukaev <aiarsanukaev@outlook.com>
2019-02-02 03:53:24 +0500

    Добавлен class Account

---
+++ SocialNetwork/Core/Account.cs
@@ -0,0 +1,43 @@
+﻿namespace SocialNetwork.Core
+{
+    /// <summary>
+    /// Инкапсулирует пользователя в социальной сети.
+    /// </summary>
+    public class Account
+    {
+        #region Вложенные структуры
+
+        #endregion
+
+        #region Поля
+
+        #endregion
+
+        #region Свойства
+
+        public string Id {
+            get;
+            protected set;
+        }
+
+        #endregion
+
+        #region Конструкторы, On-методы
+
+        public Account(string id)
+        {
+            Id = id;
+        }
+
+        #endregion
+
+        #region Методы
+
+        public override string ToString()
+        {
+            return Id;
+        }
+
+        #endregion
+    }
+}
