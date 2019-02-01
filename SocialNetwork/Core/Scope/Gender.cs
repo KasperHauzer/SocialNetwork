d7520bbd974c40b758331d0003b465153eae777b
Adam Arsanukaev <aiarsanukaev@outlook.com>
2019-02-02 02:50:26 +0500

    Добавлен enum Gender

---
+++ SocialNetwork/Core/Scope/Gender.cs
@@ -0,0 +1,27 @@
+﻿namespace SocialNetwork.Core.Scope
+{
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+
+    public static class GenderExtansion
+    {
+        public const string DefaultDescription = "NULL";
+
+        /// <summary>
+        /// Возвращает описание элемента перечислителя.
+        /// </summary>
+        /// <param name="gender">Элемент перечислителя.</param>
+        /// <returns>Описание элемента перечислителя.</returns>
+        public static string Descripte(this Gender gender)
+        {
+            switch (gender) {
+                case Gender.Female: return "женский пол";
+                case Gender.Male: return "мужской пол";
+                default: return DefaultDescription;
+            }
+        }
+    }
+}
