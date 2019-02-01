fe9c8aae69f4d0712c5a091d26549bbf4393921f
Adam Arsanukaev <aiarsanukaev@outlook.com>
2019-02-02 03:06:41 +0500

    Добавлен struct Note

---
+++ SocialNetwork/Core/Scope/Note.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace SocialNetwork.Core.Scope
+{
+    /// <summary>
+    /// Инкапсулирует запись с неизменяемыми свойствами после создания.
+    /// </summary>
+    [Serializable]
+    public struct Note : IFeedableNote
+    {
+        #region Свойства
+
+        /// <summary>
+        /// Id владельца записи.
+        /// </summary>
+        public string OwnerId {
+            get;
+        }
+
+        /// <summary>
+        /// Заголовок.
+        /// </summary>
+        public string Title {
+            get;
+        }
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public string Description {
+            get;
+        }
+
+        /// <summary>
+        /// Время создания.
+        /// </summary>
+        public DateTime Time {
+            get;
+        }
+
+        /// <summary>
+        /// Дополнительные данные.
+        /// </summary>
+        public object Data {
+            get;
+        }
+
+        #endregion
+
+        #region Конструктор
+
+        /// <summary>
+        /// Полный конструтор записи.
+        /// </summary>
+        /// <param name="ownerId">Id владельца записи.</param>
+        /// <param name="title">Заголовок.</param>
+        /// <param name="description">Описание.</param>
+        /// <param name="time">Время записи.</param>
+        /// <param name="data">Дополнительные данные записи.</param>
+        public Note(string ownerId, string title, string description, DateTime time, object data)
+        {
+            OwnerId = ownerId;
+            Title = title;
+            Description = description;
+            Time = time;
+            Data = data;
+        }
+
+        /// <summary>
+        /// Конструктор записи с установкой свойтсва <see cref="Time"/> равное времени создания записи.
+        /// </summary>
+        /// <param name="ownerId">Id владельца записи.</param>
+        /// <param name="title">Заголовок.</param>
+        /// <param name="description">Описание.</param>
+        /// <param name="data">Дополнительные данные записи.</param>
+        public Note(string ownerId, string title, string description, object data) : this(ownerId, title, description, DateTime.Now, data) { }
+
+        /// <summary>
+        /// Конструктор записи с установкой свойства <see cref="Time"/> равное времени создания записи и без дополнительных данных.
+        /// </summary>
+        /// <param name="ownerId">Id владельца записи.</param>
+        /// <param name="title">Заголовок.</param>
+        /// <param name="description">Описание.</param>
+        public Note(string ownerId, string title, string description) : this(ownerId, title, description, DateTime.Now, null) { }
+
+        #endregion
+    }
+}
